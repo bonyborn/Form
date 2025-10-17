85aa173ea33da07162206bd86d0d9b7660b90a40
Superior bonie <nzauboniface76@gmail.com>
2025-10-17 06:11:54 +0000

    initial commit

--- formapp/models.py
+++ formapp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser , BaseUserManager
 
 # Create your models here.
+class user(models.Model):
+  FullName = models.CharField(max_length=255)
+  Email = models.EmailField(unique=True)
+  phone_number = models.CharField(max_length=20)
+  password=models.CharField(max_length=30)
+  accepted_terms=models.BooleanField(default=False)
+
+def __str__(self):
+    return self,Username
